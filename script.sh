b3b85b90e6a787b43135fc6f8acb9ade7f4a128f
Roman Landik <roman.landik@gmail.com>
2022-11-09 00:44:30 +0600

    Добавил генерацию конфигов и QR

--- script.sh
+++ script.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+WIREGUARD_IP=0.0.0.0
+ETH_INTERFACE=enp0s8
+
+WIREGUARD_PORT=51667
+WIREGUARD_CONFIG=wg0.conf
+WIREGUARD_PATH=/etc/wireguard
 
 
 ufw allow OpenSSH
@@ -6,7 +13,7 @@ ufw enable
 
 add-apt-repository ppa:wireguard/wireguard
 apt update
-apt install -y wireguard bind9
+apt install -y wireguard bind9 qrencode zip
 
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 echo "net.ipv4.conf.all.rp_filter = 1" >> /etc/sysctl.conf
@@ -14,12 +21,6 @@ echo "net.ipv4.conf.default.proxy_arp = 0" >> /etc/sysctl.conf
 echo "net.ipv4.conf.default.send_redirects = 1" >> /etc/sysctl.conf
 echo "net.ipv4.conf.all.send_redirects = 0" >> /etc/sysctl.conf
 sysctl -p
-
-
-WIREGUARD_CONFIG=wg0.conf
-WIREGUARD_PATH=/etc/wireguard
-WIREGUARD_PORT=51667
-ETH_INTERFACE=enp0s8
 
 private_server=$(wg genkey)
 public_server=$(echo $private_server | wg pubkey)
@@ -34,18 +35,32 @@ echo "PrivateKey = $private_server" >> $WIREGUARD_PATH/$WIREGUARD_CONFIG
 echo "PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o $ETH_INTERFACE -j MASQUERADE; ip6tables -A FORWARD -i wg0 -j ACCEPT;" >> $WIREGUARD_PATH/$WIREGUARD_CONFIG
 echo "PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o $ETH_INTERFACE -j MASQUERADE; ip6tables -D FORWARD -i wg0 -j ACCEPT;" >> $WIREGUARD_PATH/$WIREGUARD_CONFIG
 
-for num in 10 11 12 13 14 15 16 17 18 19 20
-do
-ip=10.8.0.$num/32
-private=$(wg genkey)
-public=$(echo $private | wg pubkey)
-echo "$ip --- $private" >> keys
-echo "[peer]" >> $WIREGUARD_PATH/$WIREGUARD_CONFIG
-echo "#autogen" >> $WIREGUARD_PATH/$WIREGUARD_CONFIG
-echo "PublicKey = $public" >> $WIREGUARD_PATH/$WIREGUARD_CONFIG
-echo "AllowedIPs = $ip" >> $WIREGUARD_PATH/$WIREGUARD_CONFIG
+num=115
+while [ $num -gt 100 ]; do
+    ip=10.8.0.$num/32
+    private=$(wg genkey)
+    public=$(echo $private | wg pubkey)
+    echo "$ip --- $private" >> keys
+    echo "[peer]" >> $WIREGUARD_PATH/$WIREGUARD_CONFIG
+    echo "#autogen" >> $WIREGUARD_PATH/$WIREGUARD_CONFIG
+    echo "PublicKey = $public" >> $WIREGUARD_PATH/$WIREGUARD_CONFIG
+    echo "AllowedIPs = $ip" >> $WIREGUARD_PATH/$WIREGUARD_CONFIG
+
+    echo "[Interface]" >> client$num.conf
+    echo "PrivateKey = $private" >> client$num.conf
+    echo "Address = $ip" >> client$num.conf
+    echo "DNS = 10.8.0.1" >> client$num.conf
+    echo "" >> client$num.conf
+    echo "[Peer]" >> client$num.conf
+    echo "PublicKey = $public_server" >> client$num.conf
+    echo "Endpoint = $WIREGUARD_IP:$WIREGUARD_PORT" >> client$num.conf
+    echo "AllowedIPs = 0.0.0.0/0" >> client$num.conf
+    echo "PersistentKeepalive = 20" >> client$num.conf
+    qrencode -t ansiutf8 < client$num.conf > client$num-qr.conf
+    num=$[ $num - 1 ]
 done
 
+zip -r -9 configs.zip client*
 
 ufw allow $WIREGUARD_PORT/udp
 systemctl enable --now wg-quick@wg0
